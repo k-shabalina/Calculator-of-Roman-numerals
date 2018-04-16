d5b78bb569901bcf90818336e8107e1c032b597e
Polina <polia.tselisheva@mail.ru>
2018-04-16 21:23:01 +0500

    NewClass

---
+++ WindowsFormsApp1/WindowsFormsApp1/RimNumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class RimNumber
+    {
+        string RimN
+        {
+            get;
+            set;
+        }
+        int ArabN
+        {
+            get;
+            set;
+        }
+        //конструкторы
+        public RimNumber()//без параметров
+        {
+            RimN = "";
+            ArabN = 0;
+        }
+        public RimNumber(string RimN)
+        {
+            this.RimN = RimN;
+            ArabN = GetArabNumber(this.RimN);
+        }
+        public RimNumber(int ArabN)
+        {
+            this.ArabN = ArabN;
+            RimN = GetRimNumber;
+        }
+        int GetArabNumber(RimNumber rim)
+        {
+            int ar;
+            return ar;
+        }
+        RimNumber GetRimNumber(int ar)
+        {
+            RimNumber rim;
+            return rim;
+        }
+    }
+}
